2c7dd565e2b05e5f1538a895f1c749a50553f8b4
Vivianna12 <viviannasamite12@gmail.com>
2023-12-30 23:04:29 -0500

    added the basic handling and the typewriter effect

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipeOutput", {
+    strings: "Recipe will appear here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeForm = document.querySelector("#recipe-generator-form");
+recipeForm.addEventListener("submit", generateRecipe);
