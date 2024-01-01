6babdae2f7fb7565b73e9673b0b48087f57d0404
Vivianna12 <viviannasamite12@gmail.com>
2023-12-31 21:07:15 -0500

    added the AI api and have it working with the user's form input

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
+  console.log("generating recipe");
   new Typewriter("#recipeOutput", {
-    strings: "Recipe will appear here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+  let userInput = document.querySelector("#user-input");
+  let apiKey = "f42atbc02aod4017c837cbfe1c70ef22";
+  let prompt = `Generate a recipe about ${userInput.value}`;
+  let context =
+    "You are a food expert. Your mission is to generate a yummy recipe in basic HTML. Make sure to follow the user instructions. Sign the recipe SheCode AI in a <strong> element at the bottom ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeForm = document.querySelector("#recipe-generator-form");
